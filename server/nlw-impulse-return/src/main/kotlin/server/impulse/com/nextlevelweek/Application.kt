ae604b6e9e67eedcb048633461121a83c06828bc
Henrique Santos da Silva <45521656+Henrique-Santos-da-Silva@users.noreply.github.com>
2022-09-07 21:28:49 -0300

    changes in starting application

--- server/nlw-impulse-return/src/main/kotlin/server/impulse/com/nextlevelweek/Application.kt
+++ server/nlw-impulse-return/src/main/kotlin/server/impulse/com/nextlevelweek/Application.kt
@@ -1,14 +1,19 @@
 package server.impulse.com.nextlevelweek
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
 import server.impulse.com.nextlevelweek.database.DatabaseConfig
 import server.impulse.com.nextlevelweek.plugins.*
 import server.impulse.com.nextlevelweek.repositories.FeedbackRepositoryImpl
 import server.impulse.com.nextlevelweek.services.sendmail.SendMailImpl
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty,  port = System.getenv("PORT")?.toInt() ?: 8080, host = "0.0.0.0") {
+
+    }.start(true)
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
