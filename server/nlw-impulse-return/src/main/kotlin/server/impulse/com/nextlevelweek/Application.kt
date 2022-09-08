c5b77ce39c5cc4e07f284507b874c6c4833f3582
Henrique Santos da Silva <45521656+Henrique-Santos-da-Silva@users.noreply.github.com>
2022-09-07 21:39:50 -0300

    update in Application.kt

--- server/nlw-impulse-return/src/main/kotlin/server/impulse/com/nextlevelweek/Application.kt
+++ server/nlw-impulse-return/src/main/kotlin/server/impulse/com/nextlevelweek/Application.kt
@@ -1,6 +1,8 @@
 package server.impulse.com.nextlevelweek
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
@@ -10,9 +12,14 @@ import server.impulse.com.nextlevelweek.repositories.FeedbackRepositoryImpl
 import server.impulse.com.nextlevelweek.services.sendmail.SendMailImpl
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty,  port = System.getenv("PORT")?.toInt() ?: 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, environment = applicationEngineEnvironment {
+        config = HoconApplicationConfig(ConfigFactory.load())
 
-    }.start(true)
+        connector {
+            port = System.getenv("PORT")?.toInt() ?: 8080
+            host = "0.0.0.0"
+        }
+    }).start(true)
 }
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
